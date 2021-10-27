363ca998d5c10a0c119a42a9a7b72866eab00e93
pivarcsi.gergely <gergely.pivarcsi@gmail.com>
2021-10-27 14:36:07 +0200

    Tulajdonos + BankiSzolgaltatasok osztalyok

---
+++ src/hu/petrik/BankiSzolgaltatas.java
@@ -0,0 +1,14 @@
+package hu.petrik;
+
+public class BankiSzolgaltatas {
+
+    private Tulajdonos tulajdonos;
+
+    public BankiSzolgaltatas(Tulajdonos tulajdonos) {
+        this.tulajdonos = tulajdonos;
+    }
+
+    public Tulajdonos getTulajdonos() {
+        return tulajdonos;
+    }
+}
