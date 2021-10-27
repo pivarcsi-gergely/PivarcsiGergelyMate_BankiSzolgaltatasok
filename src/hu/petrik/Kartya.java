b951a019f0dc48895ab3cf13f004a807c2dd79bf
pivarcsi.gergely <gergely.pivarcsi@gmail.com>
2021-10-27 15:27:01 +0200

    MegtakaritasiSzamla + Kartya osztalyok

---
+++ src/hu/petrik/Kartya.java
@@ -0,0 +1,21 @@
+package hu.petrik;
+
+public class Kartya extends BankiSzolgaltatas{
+
+    private Szamla szamla;
+    private String kartyaSzam;
+
+    public Kartya(Tulajdonos tulajdonos, Szamla szamla, String kartyaSzam) {
+        super(tulajdonos);
+        this.szamla = szamla;
+        this.kartyaSzam = kartyaSzam;
+    }
+
+    public String getKartyaSzam() {
+        return kartyaSzam;
+    }
+
+    public boolean vasarlas(int osszeg){
+        return true;
+    }
+}
---
+++ src/hu/petrik/MegtakaritasiSzamla.java
@@ -0,0 +1,33 @@
+package hu.petrik;
+
+public class MegtakaritasiSzamla extends Szamla{
+
+    private double kamat;
+    public static double alapKamat;
+
+    public MegtakaritasiSzamla(Tulajdonos tulajdonos, double kamat) {
+        super(tulajdonos);
+        this.kamat = kamat;
+    }
+
+    public double getKamat() {
+        return kamat;
+    }
+
+    public void setKamat(double kamat) {
+        this.kamat = kamat;
+    }
+
+    public boolean kivesz(int osszeg){
+        if (getAktualisEgyenleg() < osszeg){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public void kamatJovairas(){
+        aktualisEgyenleg += (aktualisEgyenleg*kamat);
+    }
+}
